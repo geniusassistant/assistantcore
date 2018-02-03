d280053c7a71a27feea6a9caf16293eecc1eda6b
hexx112 <torvimmm@gmail.com>
2018-02-03 12:31:33 +0000

    Can now insert reminder into scheduler

--- cron/scheduler.py
+++ cron/scheduler.py
@@ -2,14 +2,31 @@ import say
 from threading import Timer
 import schedule
 
-def speak():
-    say.speak('are you till alive?')
+def remind(text):
+    global sched
 
-schedule.every(1000).seconds.do(speak)
+    say.speak(text)
+
+    return schedule.CancelJob
+
+def do(self, job_func, *args, **kwargs):
+    """Specifies the job_func that should be called every time the
+    job runs.
+    Any additional arguments are passed on to job_func when
+    the job runs.
+    :param job_func: The function to be scheduled
+    :return: The invoked job instance
+    """
+    self.job_func = functools.partial(job_func, *args, **kwargs)
+
+def add(time, text):
+    schedule.every().day.at(time).do(lambda: remind(text))
+
 
 def split():
     schedule.run_pending()
-    t = Timer(1.0, speak)  # every 5 seconds
+    t = Timer(1.0, split)  # every 5 seconds
     t.start()
 
+add(10, 'Remember your glasses tomorrow')
 split()
